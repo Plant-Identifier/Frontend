94aaa14b94dbb8f087a9a5c09a9d3b28a34bee9d
ritaxiang <zherita0502@gmail.com>
2024-01-13 12:20:12 -0500

    added new screen

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 // App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SplashScreen from 'react-native-splash-screen';
 
+
+import WelcomeScreen from './screens/WelcomeScreen'; // Create this component
 import CameraScreen from './screens/CameraScreen';
 import CollectionScreen from './screens/CollectionScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  useEffect(() => {
+    // Hide splash screen after your component mounts
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
+        <Tab.Screen name="Welcome" component={WelcomeScreen} />
         <Tab.Screen name="Camera" component={CameraScreen} />
         <Tab.Screen name="Collection" component={CollectionScreen} />
       </Tab.Navigator>
