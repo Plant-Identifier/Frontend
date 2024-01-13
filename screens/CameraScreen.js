09e7c5932dddf1819af0f4c57eb4e8a5b915bff2
Mona <msabetih@uwo.ca>
2024-01-13 16:03:56 -0500

    Button component created"

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Button from '../components/button';
 
 const CameraScreen = () => {
+
+  const handleButtonPress = () => {
+    console.log("Test")
+  };
+
   return (
     <View>
       <Text>Camera Screen</Text>
+      <Button onPress={handleButtonPress} title="Upload Plant" />
       {/* Add camera-related components here */}
     </View>
   );
